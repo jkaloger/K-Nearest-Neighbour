90505a49b20b838206866bdd8b254481fecdfeda
jkaloger <jkaloger@users.noreply.github.com>
2017-03-15 20:10:42 +1100

    function prototypes

---
+++ src/classifier.py
@@ -0,0 +1,18 @@
+# K-Nearest Neighbor classifier
+# Jack Kaloger 2017
+# Project 1 for COMP30027
+
+def preprocess_data(filename):
+    #
+
+def compare_instance(instance, instance, method):
+    #
+
+def get_neighbours(instance, training_data_set, k, method):
+    #
+
+def predict_class(neighbors, method):
+    #
+
+def evaluate(data_set, metric):
+    #
